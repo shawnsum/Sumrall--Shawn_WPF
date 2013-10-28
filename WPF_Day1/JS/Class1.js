ebde4a34509e2da9c6c7e2141ecb1354242b6f63
shawnsum <ShawnSum@Shawns-MacBook-Pro-2.local>
2013-10-28 11:51:20 -0400

    Numbers & Strings
    Overview of how strings and numbers work with eachother.

--- WPF_Day1/JS/Class1.js
+++ WPF_Day1/JS/Class1.js
@@ -8,7 +8,23 @@
 
 var name;
 
-name ='Shawn Sumrall is a BOSS';
+name ='Shawn Sumrall is a BOSS'; //string example
+var age = 26; //number example
+var pet = 'Waggs';
+var petAge = 11;
+
+age = age +1;
 
 console.log(name);
+console.log(age)
+age = age+1;
+console. log(age)
+age = age+10
+console.log(age)
+
+console.log(parseInt(petAge) + Number(age) + age*1);
+
+
+
+
 
